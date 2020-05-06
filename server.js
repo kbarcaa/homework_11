f06d1c9aba1fd22729f8a92cdc4663c82ccba6e3
SungJin Kim <sungjinkimm@gmail.com>
2020-05-06 17:39:16 -0400

    getNote & saveNote complete

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// === variable ====================================================
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
@@ -7,23 +7,46 @@ var PORT = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-// ===================================================
+app.use(express.static("public"));
+// === page routes ====================================================
 
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "public/index.html"))
-})
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "public/notes.html"))
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
-// ===================================================
+// === data routes ====================================================
 
-app.get("/api/notes", (req,res)=>{
-  res.json([{"title":"Test Title","text":"Test text"}])
-})
+app.get("/api/notes", (req, res) => {
+  let savedNotes = fs.readFileSync("./db/db.json");
+  savedNotes = JSON.parse(savedNotes);
+  res.json(savedNotes);
+});
 
-// Port listener
-app.listen(PORT, ()=>{
-  console.log("Web Page is live on Port: " + PORT)
-})
+app.post("/api/notes", (req, res) => {
+  
+  let savedNotes = fs.readFileSync("./db/db.json");
+  savedNotes = JSON.parse(savedNotes);
+
+  let newNote = req.body;
+  console.log(newNote);
+  console.log(savedNotes);
+
+  savedNotes.push(newNote);
+  savedNotes = JSON.stringify(savedNotes);
+  fs.writeFileSync("./db/db.json", savedNotes);
+
+  res.json({ result: "success", savedNotes });
+});
+
+// === delete route ====================================================
+
+app.delete("api/notes/:id", (req, res) => {});
+
+// === PORT ====================================================
+
+app.listen(PORT, () => {
+  console.log("Web Page is live on Port: " + PORT);
+});
